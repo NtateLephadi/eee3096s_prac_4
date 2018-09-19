ae280e83382d4ce33652bfbea5c2c32c4d77bd75
Tumelo Lephadi <lphtum003@myuct.ac.za>
2018-09-19 22:05:07 +0200

    define current values and convert

--- prac_4_lphtum003_jhrmoh002.py
+++ prac_4_lphtum003_jhrmoh002.py
@@ -2,12 +2,14 @@
 
 import spidev
 import time
-from gpiozero import MCP3008, Button, LightSensor
+from gpiozero import MCP3008, Button
 
-MCP3008(channel=0, differential=False, max_voltage=3.3, clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=8)
+MCP3008(channel=0)
 
 potentiometer=MCP3008(0)
-print(potentiometer.values)
-
+light_sensor=MCP3008(1)
 temperature_sensor=MCP3008(2)
-print(temperature_sensor.value)
+
+current_voltage=potentiometer.voltage
+current_light=light_sensor.value * 100
+current_temperature=yield(temperature_sensor.value * 3.3 - 0.5) * 100
