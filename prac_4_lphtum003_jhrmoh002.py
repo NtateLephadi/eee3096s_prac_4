4f6b88c0ab312ef5830a27ad740c008d4a2dc51c
Tumelo Lephadi <lphtum003@myuct.ac.za>
2018-09-19 22:46:32 +0200

    make buttons and spi devices

--- prac_4_lphtum003_jhrmoh002.py
+++ prac_4_lphtum003_jhrmoh002.py
@@ -10,6 +10,25 @@ potentiometer=MCP3008(0)
 light_sensor=MCP3008(1)
 temperature_sensor=MCP3008(2)
 
+reset=Button(17)
+frequency=Button(18)
+stop=Button(27)
+display=Button(22)
+
 current_voltage=potentiometer.voltage
 current_light=light_sensor.value * 100
 current_temperature=yield(temperature_sensor.value * 3.3 - 0.5) * 100
+
+print("-10%s -10%s -10%s -10%s -10%s" % ("Time", "Timer", "Pot", "Temp", "Light"))
+
+while True:
+    while(reset.is_pressed):
+        continue
+    while(frequency.is_pressed):
+        continue
+    while(stop.is_pressed):
+        continue
+    while(display.is_pressed):
+        continue
+    
+    
